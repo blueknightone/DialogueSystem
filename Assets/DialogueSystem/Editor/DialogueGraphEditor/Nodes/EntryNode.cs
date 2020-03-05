d0bc45ebe6cd5633df05628b3ca2294688e19d3e
Justin Abbott <jabbott@pershingcounty.net>
2020-03-05 15:35:20 -0800

    Add comments to EntryNode.cs.cs

--- Assets/DialogueSystem/Editor/DialogueGraphEditor/Nodes/EntryNode.cs
+++ Assets/DialogueSystem/Editor/DialogueGraphEditor/Nodes/EntryNode.cs
@@ -1,5 +1,8 @@
 ﻿namespace lastmilegames.DialogueSystem.DialogueGraphEditor.Nodes
 {
+    /// <summary>
+    /// Represents the empty starting point for each Dialogue.
+    /// </summary>
     public class EntryNode : BaseNode
     {
         public EntryNode()
