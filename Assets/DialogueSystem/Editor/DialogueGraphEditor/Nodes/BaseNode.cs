6d88191423e9b610734f2360fc9269a8aee9586c
Justin Abbott <jabbott@pershingcounty.net>
2020-03-05 14:20:18 -0800

    Add comments and cleanup BaseNode.cs

--- Assets/DialogueSystem/Editor/DialogueGraphEditor/Nodes/BaseNode.cs
+++ Assets/DialogueSystem/Editor/DialogueGraphEditor/Nodes/BaseNode.cs
@@ -5,11 +5,25 @@ using UnityEngine.UIElements;
 
 namespace lastmilegames.DialogueSystem.DialogueGraphEditor.Nodes
 {
+    /// <summary>
+    /// The base class all DialogueGraphEditor nodes inherit from.
+    /// </summary>
     public abstract class BaseNode : Node
     {
+        /// <summary>
+        /// The default minimum size of a node.
+        /// Nodes will expand based on USS rules.
+        /// </summary>
         public static readonly Vector2 DefaultNodeSize = new Vector2(150, 200);
+
+        /// <summary>
+        /// The node's unique identifier. 
+        /// </summary>
         public string GUID { get; protected set; }
 
+        /// <summary>
+        /// The action delegate to call when the remove port button is clicked.
+        /// </summary>
         protected readonly Action<Node, Port> OnClickRemovePort;
 
         /// <summary>
@@ -18,9 +32,17 @@ namespace lastmilegames.DialogueSystem.DialogueGraphEditor.Nodes
         protected BaseNode()
         {
             GUID = Guid.NewGuid().ToString();
-            
-            GeneratePort(this,"Input", Direction.Input, Port.Capacity.Multi);
+
+            GeneratePort(this, "Input", Direction.Input, Port.Capacity.Multi);
             inputContainer.Q<Port>().portColor = Color.cyan;
+            OnClickRemovePort = (node, port) =>
+            {
+                Debug.LogWarningFormat(
+                    "No remove port action set on node {0} port {1}",
+                    port.parent.IndexOf(port),
+                    GUID
+                );
+            };
         }
 
         /// <summary>
@@ -31,13 +53,23 @@ namespace lastmilegames.DialogueSystem.DialogueGraphEditor.Nodes
         {
             GUID = Guid.NewGuid().ToString();
             OnClickRemovePort = onClickRemovePort;
-            
+
             // Create input port
-            GeneratePort(this,"Input", Direction.Input, Port.Capacity.Multi);
+            GeneratePort(this, "Input", Direction.Input, Port.Capacity.Multi);
             inputContainer.Q<Port>().portColor = Color.cyan;
         }
-        
-        public Port GeneratePort(Node node, string portName, Direction portDirection, Port.Capacity capacity 
+
+        /// <summary>
+        /// Generates a new standard GraphView.Port
+        /// </summary>
+        /// <param name="node">Node to add port to.</param>
+        /// <param name="portName">Name of the port.</param>
+        /// <param name="portDirection">Whether the port is an input or output port.</param>
+        /// <param name="capacity">Whether the port can only accept one connection or multiple.</param>
+        /// <param name="type">The value type of the port.</param>
+        /// <returns>Return the generated port,</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when an invalid direction is provided.</exception>
+        public Port GeneratePort(Node node, string portName, Direction portDirection, Port.Capacity capacity
             = Port.Capacity.Single, Type type = null)
         {
             Port port = node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, type);
@@ -54,7 +86,7 @@ namespace lastmilegames.DialogueSystem.DialogueGraphEditor.Nodes
                 default:
                     throw new ArgumentOutOfRangeException(nameof(portDirection), portDirection, null);
             }
-            
+
             return port;
         }
     }
